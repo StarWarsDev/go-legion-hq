586ba07ce099ab64b312df739844a34cb413376e
Steve Good <sgood@lanctr.com>
2020-11-06 13:04:32 -0800

    Swapped data to pull from new JS files

--- data.go
+++ data.go
@@ -5,27 +5,125 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
+
+	"rogchap.com/v8go"
 )
 
-// RepoURL is the URL to the github repo
-const RepoURL = "https://raw.githubusercontent.com/NicholasCBrown/legion-HQ-2.0"
+// RootDataURL is the base URL for all data files
+const RootDataURL = "https://raw.githubusercontent.com/NicholasCBrown/legion-hq-web/master/src/constants"
 
-// GetData retrieves a copy of src/data.json from the master branch
+// CardDataJS is the JavaScript file that defines the card data for LegionHQ
+const CardDataJS = "cards.js"
+
+// KeywordDataJS is the JavaScript file that defines the keyword data for LegionHQ
+const KeywordDataJS = "keywords.js"
+
+// GetData retrieves all data from files in the master branch
 func GetData() (Data, error) {
-	dataURL := RepoURL + "/master/src/data.json"
-	var data Data
+	data := Data{
+		AllCards:       map[string]Card{},
+		KeywordDict:    map[string]string{},
+		CommunityLinks: map[string][]Link{},
+	}
+
+	// cards
+	cardsJSON, err := execCardsJS()
+	if err != nil {
+		return data, err
+	}
+	var cards map[string]interface{}
+	_ = json.Unmarshal([]byte(cardsJSON), &cards)
+	for key := range cards {
+		cardMap := cards[key]
+		cardBytes, _ := json.Marshal(&cardMap)
+		card := jsonToCard(string(cardBytes))
+		data.AllCards[card.ID] = card
+	}
+
+	// TODO: keywords
+	keywordsJSON, err := execKeywordsJS()
+	if err != nil {
+		return data, err
+	}
+	var keywords map[string]string
+	_ = json.Unmarshal([]byte(keywordsJSON), &keywords)
+	data.KeywordDict = keywords
+
+	return data, nil
+}
+
+func getScript(script string) ([]byte, error) {
+	url := fmt.Sprintf("%s/%s", RootDataURL, script)
 	client := http.Client{}
-	resp, err := client.Get(dataURL)
+	resp, err := client.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return []byte{}, fmt.Errorf("non-Ok response code %s (%d)", resp.Status, resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
+
+func execJS(scriptBytes []byte, varName string) (*v8go.Value, error) {
+	// prepare the script
+	script := strings.Replace(
+		string(scriptBytes),
+		fmt.Sprintf("export default %s;", varName),
+		"",
+		1,
+	)
+
+	ctx, _ := v8go.NewContext(nil)
+	ctx.RunScript(script, "data.js")
+	val, err := ctx.RunScript(fmt.Sprintf("JSON.stringify(%s)", varName), "value.js")
+	return val, err
+}
+
+func execCardsJS() (string, error) {
+	var data string
+	scriptBytes, err := getScript(CardDataJS)
 	if err != nil {
 		return data, err
 	}
 
-	if resp.StatusCode != 200 {
-		return data, fmt.Errorf("non-Ok response code %s (%d)", resp.Status, resp.StatusCode)
+	val, err := execJS(scriptBytes, "cards")
+	if err != nil {
+		return data, err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	_ = json.Unmarshal(body, &data)
+	data = val.String()
 
 	return data, nil
+}
+
+func execKeywordsJS() (string, error) {
+	var data string
+	scriptBytes, err := getScript(KeywordDataJS)
+	if err != nil {
+		return data, err
+	}
+
+	val, err := execJS(scriptBytes, "keywords")
+	if err != nil {
+		return data, err
+	}
+
+	data = val.String()
+
+	return data, nil
+}
+
+func jsonToCard(cardJSON string) Card {
+	var card Card
+
+	cardBytes := []byte(cardJSON)
+	_ = json.Unmarshal(cardBytes, &card)
+
+	return card
 }
---
+++ data_test.go
@@ -0,0 +1,125 @@
+package legionhq
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+const darthVaderCommanderJSON = `{
+		"cardType": "unit",
+		"defense": "red",
+		"surges": [],
+		"speed": 1,
+		"wounds": 8,
+		"courage": -1,
+		"cardSubtype": "trooper",
+		"cardName": "Darth Vader",
+		"title": "Dark Lord of the Sith",
+		"isUnique": true,
+		"rank": "commander",
+		"cost": 190,
+		"faction": "empire",
+		"imageName": "Darth Vader.jpeg",
+		"keywords": [
+			"Deflect",
+			"Immune",
+			"Master of the Force",
+			"Relentless",
+			"Impact",
+			"Pierce"
+		],
+		"upgradeBar": ["force", "force", "force"],
+		"history": [
+			{
+				"date": "12 September 2019",
+				"description": "Cost reduced from 200 to 190 points."
+			}
+		],
+		"id": "at"
+	}`
+
+func Test_GetData(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{name: "get data", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetData()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			dataJSON, _ := json.Marshal(&got)
+			t.Log(string(dataJSON))
+		})
+	}
+}
+
+func Test_execCardsJS(t *testing.T) {
+	tests := []struct {
+		name               string
+		wantLenGreaterThan int
+		wantErr            bool
+	}{
+		{name: "data js", wantLenGreaterThan: 0, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := execCardsJS()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("execCardsJS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) <= tt.wantLenGreaterThan {
+				t.Errorf("execCardsJS() got = %v, want > %v", len(got), tt.wantLenGreaterThan)
+			}
+		})
+	}
+}
+
+func Test_jsonToCard(t *testing.T) {
+	type args struct {
+		cardJSON string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Card
+	}{
+		{
+			name: "Darth Vader Commander",
+			args: struct{ cardJSON string }{cardJSON: darthVaderCommanderJSON},
+			want: Card{
+				ID:          "at",
+				CardName:    "Darth Vader",
+				CardType:    "unit",
+				CardSubType: "trooper",
+				Cost:        190,
+				Faction:     "empire",
+				IsUnique:    true,
+				Keywords: []string{
+					"Deflect",
+					"Immune",
+					"Master of the Force",
+					"Relentless",
+					"Impact",
+					"Pierce",
+				},
+				Rank:       "commander",
+				UpgradeBar: []string{"force", "force", "force"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jsonToCard(tt.args.cardJSON); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("jsonToCard() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
